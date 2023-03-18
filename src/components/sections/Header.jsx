54737a063ca475cea1ee0c60cbfa8c0c0b32aa56
redfox <76628594+purplehippo911@users.noreply.github.com>
2023-03-18 22:01:10 +0100

    halfway done with header, needs state management and some extra styles 🔨

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
+import MobileNav from "../MobileNav";
 
 function Header() {
+
+    const [isToggled, setIsToggled] = useState(false);
+
+    function toggleMobileNav() {
+        setIsToggled(true);
+        console.log("toggled");
+    }
+
     return ( 
-        <header className="py-4 px-4 bg-red-300">
+        <header className="py-4 px-4">
             <div className="container flex justify-center">
-                <img src="../images/logo-bookmark.svg" alt="bookmark logo"
+                <img src="../images/logo-bookmark.svg" alt="bookmark logo" className="mr-auto"
                 />
+
+                <nav className="hidden sm:block">
+                    <ul className="nav__list flex gap-4 px-2">
+                        <li className="nav__item">Features</li>
+                        <li className="nav__item">Pricing</li>
+                        <li className="nav__item">Contact</li>
+                        <li className="nav__item bg-softRed text-white px-4">Login</li>
+                    </ul>
+                </nav>
+
                 <img src="../images/icon-hamburger.svg" alt="hamburger"
-                    className="ml-auto"
+                    className="ml-auto sm:hidden" onClick={toggleMobileNav}
                 />
+            </div>
+            <div className="container flex">
+                { isToggled && <MobileNav/>  }
             </div>
         </header>
     );
