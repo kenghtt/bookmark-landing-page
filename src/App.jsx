9d8ccf983e2535805fd524a6848da6f3f9ff5057
redfox <76628594+purplehippo911@users.noreply.github.com>
2023-03-19 20:47:59 +0100

    started on hero🔨

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import React from "react";
 // components
 import Header from "./components/sections/Header";
 import Footer from "./components/sections/Footer";
+import Main from "./components/sections/Main";
+
+// styles
 import "./index.css";
 
 
@@ -11,6 +14,7 @@ function App() {
   return (
     <div className="App">
         <Header/>
+        <Main/>
         <Footer/>
     </div>
   )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,14 +7,16 @@ module.exports = {
     './src/*.jsx',
   ],
   theme: {
-    colors: {
+    
+    extend: {
+      colors: {
       'softBlue':'hsl(231, 69%, 60%)',
       'softRed':'hsl(0, 94%, 66%)',
       'grayBlue':'hsl(229, 8%, 60%)',
       'veryDarkBlue':'hsl(229, 31%, 21%)',
       'white':"#FFF"
     },
-    extend: {},
+  },
   },
   plugins: [],
 }
