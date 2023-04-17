92c048fd735cd948a51135b6f50eb2db813e5593
Jeremy <kenghtt@gmail.com>
2023-04-16 23:09:07 -0500

    Add validator library

--- src/components/layout/ContactUsForm/ContactUsForm.jsx
+++ src/components/layout/ContactUsForm/ContactUsForm.jsx
@@ -1,5 +1,22 @@
-function ContactUsForm() {
+import {useState} from "react";
+import validator from "validator";
 
+function ContactUsForm() {
+    const [isValidEmail, setIsValidEmail] = useState(true);
+
+    const validateEmail = (e) => {
+        var email = e.target.parentElement.querySelector('input').value
+
+
+        if (validator.isEmail(email)) {
+            setIsValidEmail(true);
+            console.log('Email Valid')
+        } else {
+            setIsValidEmail(false);
+            console.log('Email Invalid')
+
+        }
+    };
     return (
         <div className="bg-softBlue   mt-[10%] w-auto">
             <div className="grid grid-rows-3 text-center pb-[5%] ">
@@ -9,11 +26,13 @@ function ContactUsForm() {
                 <div className="mt-14">
                     <h1 className="text-white font-medium ">Stay up-to-date with what we're doing</h1>
                 </div>
-                <section className="mt-14">
-                    <form>
+                <section className="mt-14 h-[100%]" onSubmit='return false'>
+                    <form className='h-[100%]'>
                         <input type="email" className="py-2 px-2 border-2 border-white rounded mr-3" id="lname"
-                               name="lname" placeholder="Enter your email address"/>
-                        <button type='submit' className="py-2 btn btn-primary self-center bg-red-500 ">Contact Us
+                               name="lname" placeholder="Enter your email address" />
+                        {/*<span className="text-xs text-red-700" id="passwordHelp">Your password is too short.</span>*/}
+
+                        <button type='button'  onClick={e => validateEmail(e)} className="py-2 btn btn-primary self-center bg-red-500 ">Contact Us
                         </button>
                     </form>
                 </section>
