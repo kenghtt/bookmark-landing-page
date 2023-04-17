46cdaa1335a98ddf81766b95e85487d3697a8ad0
Jeremy <kenghtt@gmail.com>
2023-04-17 01:40:59 -0500

    Add validation

--- src/components/layout/ContactUsForm/ContactUsForm.jsx
+++ src/components/layout/ContactUsForm/ContactUsForm.jsx
@@ -4,9 +4,10 @@ import validator from "validator";
 function ContactUsForm() {
     const [isValidEmail, setIsValidEmail] = useState(true);
 
+    let emailBorder = isValidEmail ? 'border-white' : 'border-red-500';
+    let rounded = isValidEmail ? 'rounded' : 'rounded-t';
     const validateEmail = (e) => {
-        var email = e.target.parentElement.querySelector('input').value
-
+        var email = e.target.parentElement.parentElement.querySelector('input').value
 
         if (validator.isEmail(email)) {
             setIsValidEmail(true);
@@ -19,21 +20,32 @@ function ContactUsForm() {
     };
     return (
         <div className="bg-softBlue   mt-[10%] w-auto">
-            <div className="grid grid-rows-3 text-center pb-[5%] ">
+            <div className="grid grid-rows-3 text-center pb-[3%] ">
                 <div className="mt-20">
                     <p className="text-white">35,000+ ALREADY JOINED</p>
                 </div>
-                <div className="mt-14">
+                <div className="mt-10">
                     <h1 className="text-white font-medium ">Stay up-to-date with what we're doing</h1>
                 </div>
-                <section className="mt-14 h-[100%]" onSubmit='return false'>
-                    <form className='h-[100%]'>
-                        <input type="email" className="py-2 px-2 border-2 border-white rounded mr-3" id="lname"
-                               name="lname" placeholder="Enter your email address" />
-                        {/*<span className="text-xs text-red-700" id="passwordHelp">Your password is too short.</span>*/}
+                <section className="mt-5 h-[100%]" onSubmit='return false'>
+                    <form className='flex flex-row h-[100%] justify-center'>
+                        <div className="grid grid-rows-2 relative self-center mr-3">
+                            <input type="email" className={`py-2 pl-2 pr-[50px] border-2 border-white ${rounded} ${emailBorder}`}
+                                   id="lname" name="lname" placeholder="Enter your email address"/>
+                            <img className='absolute right-2 top-3' src="../images/icon-error.svg" alt="error icon"/>
 
-                        <button type='button'  onClick={e => validateEmail(e)} className="py-2 btn btn-primary self-center bg-red-500 ">Contact Us
-                        </button>
+                            {
+                                !isValidEmail &&
+                                <span className="text-xs text-white bg-red-500 h-5 text-left pl-2" id="passwordHelp">Whoops, make sure it's an email</span>
+
+                            }
+                        </div>
+                        <div className="grid grid-rows-2 self-center">
+                            <button type='button' onClick={e => validateEmail(e)}
+                                    className="py-2 btn btn-primary self-center bg-red-500 border-2 border-red-500 hover:text-red-500 ">
+                                Contact Us
+                            </button>
+                        </div>
                     </form>
                 </section>
             </div>
