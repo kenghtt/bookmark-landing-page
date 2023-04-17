ce88f76d40dcf24c4940dab8252c43b0ee27eba1
Jeremy <kenghtt@gmail.com>
2023-04-17 02:27:31 -0500

    Fix email form for mobile

--- src/components/layout/ContactUsForm/ContactUsForm.jsx
+++ src/components/layout/ContactUsForm/ContactUsForm.jsx
@@ -21,28 +21,32 @@ function ContactUsForm() {
     return (
         <div className="bg-softBlue   mt-[10%] w-auto">
             <div className="grid grid-rows-3 text-center pb-[3%] ">
-                <div className="mt-20">
+                <div className="mt-28 md:mt-20">
                     <p className="text-white">35,000+ ALREADY JOINED</p>
                 </div>
                 <div className="mt-10">
-                    <h1 className="text-white font-medium ">Stay up-to-date with what we're doing</h1>
+                    <h1 className="text-white font-medium text-4xl ">Stay up-to-date with what we're doing</h1>
                 </div>
                 <section className="mt-5 h-[100%]" onSubmit='return false'>
-                    <form className='flex flex-row h-[100%] justify-center'>
-                        <div className="grid grid-rows-2 relative self-center mr-3">
-                            <input type="email" className={`py-2 pl-2 pr-[50px] border-2 border-white ${rounded} ${emailBorder}`}
+                    <form className='grid sm:grid-rows-2  md:flex md:flex-row md:h-[100%] justify-center'>
+                        <div className="grid grid-rows-2 relative self-center md:mr-3">
+                            <input type="email"
+                                   className={`py-2 pl-3 pr-[50px] border-2 border-white ${rounded} ${emailBorder}`}
                                    id="lname" name="lname" placeholder="Enter your email address"/>
-                            <img className='absolute right-2 top-3' src="../images/icon-error.svg" alt="error icon"/>
 
                             {
                                 !isValidEmail &&
-                                <span className="text-xs text-white bg-red-500 h-5 text-left pl-2" id="passwordHelp">Whoops, make sure it's an email</span>
-
+                                [
+                                    <span className="text-xs text-white bg-red-500 h-5 text-left pl-3 italic"
+                                          id="passwordHelp">Whoops, make sure it's an email</span>,
+                                    <img className='absolute right-2 top-3' src="../images/icon-error.svg"
+                                         alt="error icon"/>
+                                ]
                             }
                         </div>
-                        <div className="grid grid-rows-2 self-center">
+                        <div className="grid grid-rows-2 self-center bottom-3 md:bottom-0 relative">
                             <button type='button' onClick={e => validateEmail(e)}
-                                    className="py-2 btn btn-primary self-center bg-red-500 border-2 border-red-500 hover:text-red-500 ">
+                                    className="py-2 btn btn-primary self-center bg-red-500  border-2 border-red-500 hover:text-red-500 ">
                                 Contact Us
                             </button>
                         </div>
